df042fc7828eeaced74d2da00aa08efbca9ab72f
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-10-17 00:09:20 +0200

    Add files via upload

---
+++ KlasaB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciZaVjezbu2
+{
+    internal class KlasaB
+    {
+        public string BezPrvogIZadnjeg(string stringUnos)
+        {
+            return stringUnos.Substring(1, stringUnos.Length - 2);
+
+        }
+    }
+}
